530d1b37ae54055cb8727227a621f5b20f6ca714
Berk Çapar <berkcapar@gmail.com>
2021-01-07 21:53:52 +0300

    concert page added

--- pages/live.js
+++ pages/live.js
@@ -1,10 +1,20 @@
-import Layout from "../components/Layout"
+import Footer from '../components/Footer'
+import Layout from '../components/Layout'
 
 const Live = () => {
-    return (
-        <Layout/>
-    )
-
+  return (
+    <div>
+      <Layout />
+      <h2>NO CONCERTS AT THE MOMENT </h2>
+      <Footer />
+      <style jsx>{`
+        h2 {
+          text-align: center;
+          margin-bottom: 3rem;
+        }
+      `}</style>
+    </div>
+  )
 }
 
 export default Live
