6f60b4ca48261ea091b20a9d5028a33576e5dc1d
Berk Çapar <berkcapar@gmail.com>
2021-01-07 22:03:26 +0300

    social media icons fix

--- components/Footer.js
+++ components/Footer.js
@@ -6,19 +6,19 @@ const Footer = () => {
       <h3 className="follow-text">Go Follow Me!</h3>
       <div className="icons">
         <a href="https://www.instagram.com/denizerden/">
-          <i class="fab fa-instagram fa-2x"></i>
+          <i className="fab fa-instagram fa-2x"></i>
         </a>
         <a href="https://www.youtube.com/channel/UCaKjcv9ns0qRCicbkjmxIuA">
-          <i class="fab fa-youtube fa-2x"></i>
+          <i className="fab fa-youtube fa-2x"></i>
         </a>
         <a href="https://open.spotify.com/artist/6ugwBDrWkTel2S01VrT72I?si=e1s2MoFnQg-wfXLlhBBG1w">
-          <i class="fab fa-spotify fa-2x"></i>
+          <i className="fab fa-spotify fa-2x"></i>
         </a>
         <a href="https://soundcloud.com/denizerden">
-          <i class="fab fa-soundcloud fa-2x"></i>
+          <i className="fab fa-soundcloud fa-2x"></i>
         </a>
         <a href="https://www.facebook.com/Deniz-Erden-2222409948040540">
-          <i class="fab fa-facebook fa-2x"></i>
+          <i className="fab fa-facebook fa-2x"></i>
         </a>
       </div>
       <style jsx>{`
